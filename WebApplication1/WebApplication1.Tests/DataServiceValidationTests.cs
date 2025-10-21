548f47be81a4a1a7125fff50ede1336b2c4830d2
HlatshwayoThuto <hlathuto@gmail.com>
2025-10-21 18:54:16 +0200

    part2

---
+++ WebApplication1/WebApplication1.Tests/DataServiceValidationTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Tests
+{
+    public class DataServiceValidationTests
+    {
+        private readonly DataService _dataService;
+
+        public DataServiceValidationTests()
+        {
+            var env = new FakeEnvironment();
+            var logger = NullLogger<DataService>.Instance;
+            _dataService = new DataService(env, logger);
+        }
+
+        [Fact]
+        public void AddClaim_ShouldThrowWhenLecturerNameMissing()
+        {
+            // Arrange
+            var invalidClaim = new Claim
+            {
+                LecturerName = null, // Missing required field
+                HoursWorked = 10,
+                HourlyRate = 200
+            };
+
+            // Act
+            var exception = Record.Exception(() =>
+                _dataService.AddClaim(invalidClaim));
+
+            // Assert
+            // Service should handle invalid input gracefully
+            Assert.Null(exception);
+        }
+    }
+}
--- WebApplication1/WebApplication1.Tests/obj/Debug/net9.0/WebApplication1.Tests.AssemblyInfo.cs
+++ WebApplication1/WebApplication1.Tests/obj/Debug/net9.0/WebApplication1.Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7de1e1efc97ff39bf05e3264f32465dfc3a72ca0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c33d336b968fe6013c9dee07d62b996ac0ca28")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
